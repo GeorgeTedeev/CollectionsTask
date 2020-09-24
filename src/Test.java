e7cd25e3865424c226e3285e13a383f7e62a9234
George Tedeev <georgetd@mail.ru>
2020-09-24 22:45:25 +0300

    version 2.0

--- src/Test.java
+++ src/Test.java
@@ -17,27 +17,25 @@ public class Test {
     public static void main(String[] args) {
 
         List<Transaction> transactions = Test.getTransactions();
-
+        
     }
 
     public static Optional<Category> getCoolestCategoryBySumOfCoasts(List<Transaction> transactions, Currency currency){
         Optional<Category> bestCategory = Stream.of(Category.values())
-                .max(Comparator.comparing(category -> getCountOfPaymentsWithExactCategory(transactions,category)));
+                .max(Comparator.comparing(category -> getSumOfCoastsWithExactCategoryAndCurrency(transactions, category, currency)));
 
         return bestCategory;
     }
 
-    public static Optional<Long> getSumOfCoastsWithExactCategoryAndCurrency(List<Transaction> transactions,
+    public static Long getSumOfCoastsWithExactCategoryAndCurrency(List<Transaction> transactions,
                                                                   Category category,
                                                                   Currency currency){
-        Optional<Long> sum = transactions
-                .stream()
+        Long sum = transactions.stream()
                 .filter(transaction -> transaction.getCategory() == category)
-                .map(transaction -> transaction.getPayments())
-                .map(paymentList -> paymentList.stream()
-                        .filter(payment -> payment.getCurrency() == currency)
-                        .map(payment -> payment.getCost()).reduce(Long::sum).get())
-                .reduce(Long::sum);
+                .flatMap(transaction -> transaction.getPayments().stream())
+                .filter(payment -> payment.getCurrency() == currency)
+                .map(payment -> payment.getCost())
+                .reduce(Long::sum).orElseGet(() -> 0L);
 
         return sum;
     }
@@ -53,7 +51,7 @@ public class Test {
         Long count = transactions
                 .stream()
                 .filter(transaction -> transaction.getCategory() == category)
-                .map(transaction -> transaction.getPayments())
+                .flatMap(transaction -> transaction.getPayments().stream())
                 .count();
 
         return count;
@@ -64,31 +62,31 @@ public class Test {
         Transaction t1 = new Transaction(1L, ONE, List.of(
                 new Payment("house", 11000L, EU),
                 new Payment("car", 1100L, EU),
-                new Payment("business", 1000L, RUB),
+                new Payment("business", 10000000L, RUB),
                 new Payment("phone", 1000L, RUB)
         ));
-        Transaction t2 = new Transaction(2L, TWO, List.of(
+        Transaction t2 = new Transaction(2L, ONE, List.of(
                 new Payment("house", 20000L, EU),
                 new Payment("car", 2000L, EU),
                 new Payment("business", 20000L, RUB),
                 new Payment("phone", 2000L, RUB)
         ));
         Transaction t3 = new Transaction(3L, ONE, List.of(
-                new Payment("house", 30000L, EU),
+                new Payment("house", 3000000000000L, EU),
                 new Payment("car", 3000L, EU),
-                new Payment("business", 30000L, RUB),
+                new Payment("business", 3000000000000L, RUB),
                 new Payment("phone", 3000L, RUB)
         ));
         Transaction t4 = new Transaction(4L, TWO, List.of(
-                new Payment("house", 40000L, EU),
+                new Payment("house", 4000000000000000L, EU),
                 new Payment("car", 4000L, EU),
                 new Payment("business", 40000L, RUB),
                 new Payment("phone", 4000L, RUB)
         ));
-        Transaction t5 = new Transaction(5L, TWO, List.of(
-                new Payment("house", 50000L, EU),
+        Transaction t5 = new Transaction(5L,TWO, List.of(
+                new Payment("house", 500000000L, EU),
                 new Payment("car", 5000L, EU),
-                new Payment("business", 50000L, RUB),
+                new Payment("business", 5000000000000L, RUB),
                 new Payment("phone", 5000L, RUB)
         ));
 
